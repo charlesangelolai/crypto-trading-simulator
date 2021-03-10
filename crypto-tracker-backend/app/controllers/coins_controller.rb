6ccb31774281680da8cc3d430fb4f87c031dc89c
Charles Lai <charlesangeloplai@gmail.com>
2021-03-10 02:18:58 -0500

    adds user and coin controllers

---
+++ crypto-tracker-backend/app/controllers/coins_controller.rb
@@ -0,0 +1,51 @@
+class CoinsController < ApplicationController
+  before_action :set_coin, only: [:show, :update, :destroy]
+
+  # GET /coins
+  def index
+    @coins = Coin.all
+
+    render json: @coins
+  end
+
+  # GET /coins/1
+  def show
+    render json: @coin
+  end
+
+  # POST /coins
+  def create
+    @coin = Coin.new(coin_params)
+
+    if @coin.save
+      render json: @coin, status: :created, location: @coin
+    else
+      render json: @coin.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /coins/1
+  def update
+    if @coin.update(coin_params)
+      render json: @coin
+    else
+      render json: @coin.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /coins/1
+  def destroy
+    @coin.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_coin
+      @coin = Coin.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def coin_params
+      params.require(:coin).permit(:name, :sym, :logo, :bought_at, :current_price)
+    end
+end
