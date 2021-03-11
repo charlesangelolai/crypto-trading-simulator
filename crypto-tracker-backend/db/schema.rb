9a7736342fdb8d22a6e536437c9c12133f7a371c
Charles Lai <charlesangeloplai@gmail.com>
2021-03-11 12:19:07 -0500

    updates database schema

---
+++ crypto-tracker-backend/db/schema.rb
@@ -0,0 +1,49 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_03_10_172205) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "coins", force: :cascade do |t|
+    t.string "name"
+    t.string "sym"
+    t.string "logo"
+    t.decimal "price"
+    t.decimal "hourly_percentage_change"
+    t.decimal "high"
+    t.decimal "low"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "trades", force: :cascade do |t|
+    t.integer "qty"
+    t.decimal "cost"
+    t.decimal "value"
+    t.decimal "profit"
+    t.integer "user_id"
+    t.integer "coin_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.decimal "buying_power", default: "100000.0"
+    t.decimal "wallet_value", default: "0.0"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
