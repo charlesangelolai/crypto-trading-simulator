d07e25d1dedafafe669b0eec238f8dae7f3ce947
Charles Lai <charlesangeloplai@gmail.com>
2021-03-11 12:18:49 -0500

    updates user controller

--- crypto-tracker-backend/app/controllers/users_controller.rb
+++ crypto-tracker-backend/app/controllers/users_controller.rb
@@ -46,6 +46,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :buying_power, :portfolio_value)
+      params.require(:user).permit(:username)
     end
 end
