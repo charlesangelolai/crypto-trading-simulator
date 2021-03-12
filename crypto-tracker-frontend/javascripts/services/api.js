b7ff5824b7b26b40e9b8f66c3efb9cb8bafdd8a1
Charles Lai <charlesangeloplai@gmail.com>
2021-03-12 04:48:48 -0500

    updates api.js

---
+++ crypto-tracker-frontend/javascripts/services/api.js
@@ -0,0 +1,54 @@
+class Api {
+  static baseUrl = "http://localhost:3000";
+
+  static headers = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  };
+
+  static async get(path) {
+    let resp = await fetch(Api.baseUrl + path, {
+      method: "GET",
+      headers: Api.headers,
+    });
+
+    let data = await resp.json();
+
+    return data;
+  }
+
+  static async post(path, params) {
+    let resp = await fetch(Api.baseUrl + path, {
+      method: "POST",
+      headers: Api.headers,
+      body: JSON.stringify(params),
+    });
+
+    let data = await resp.json();
+
+    return data;
+  }
+
+  static async patch(path, params) {
+    let resp = await fetch(Api.baseUrl + path, {
+      method: "PATCH",
+      headers: Api.headers,
+      body: JSON.stringify(params),
+    });
+
+    let data = await resp.json();
+
+    return data;
+  }
+
+  static async delete(path) {
+    let resp = await fetch(Api.baseUrl + path, {
+      method: "DELETE",
+      headers: Api.headers,
+    });
+
+    let data = await resp.json();
+
+    return data;
+  }
+}
