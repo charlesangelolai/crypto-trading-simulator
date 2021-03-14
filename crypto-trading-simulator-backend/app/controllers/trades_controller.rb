c7beca620af3b816b06be2e087a9d54c0a66c9b8
Charles Lai <charlesangeloplai@gmail.com>
2021-03-14 01:14:46 -0500

    updates trades controller

--- crypto-trading-simulator-backend/app/controllers/trades_controller.rb
+++ crypto-trading-simulator-backend/app/controllers/trades_controller.rb
@@ -4,13 +4,12 @@ class TradesController < ApplicationController
   # GET /trades
   def index
     @trades = Trade.all
-
-    render json: @trades
+    render json: TradeSerializer.new(@trades, {include: [:user]})
   end
-
+  
   # GET /trades/1
   def show
-    render json: @trade
+    render json: TradeSerializer.new(@trade, {include: [:user]})
   end
 
   # POST /trades
@@ -18,7 +17,8 @@ class TradesController < ApplicationController
     @trade = Trade.new(trade_params)
 
     if @trade.save
-      render json: @trade, status: :created, location: @trade
+      # render json: @trade, status: :created, location: @trade, except: [:created_at, :updated_at], include: [:user]
+      render json: TradeSerializer.new(@trade, {include: [:user]}), status: :created, location: @trade
     else
       render json: @trade.errors, status: :unprocessable_entity
     end
@@ -27,7 +27,7 @@ class TradesController < ApplicationController
   # PATCH/PUT /trades/1
   def update
     if @trade.update(trade_params)
-      render json: @trade
+      render json: @trade, except: [:created_at, :updated_at], include: [:user]
     else
       render json: @trade.errors, status: :unprocessable_entity
     end
@@ -46,6 +46,6 @@ class TradesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def trade_params
-      params.require(:trade).permit(:qty, :cost, :value, :profit)
+      params.require(:trade).permit(:qty, :cost, :logo, :sym, :qty, :coin_id, :user_id)
     end
 end
