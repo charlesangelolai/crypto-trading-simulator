83d777b601b120b65be694373ce9c3fc4fa895c5
Charles Lai <charlesangeloplai@gmail.com>
2021-03-14 01:29:37 -0500

    updates user.js

--- crypto-trading-simulator-frontend/javascripts/models/user.js
+++ crypto-trading-simulator-frontend/javascripts/models/user.js
@@ -1,99 +1,87 @@
 class User {
   static all = [];
-  static current_user;
+  static current_user = null;
 
   constructor(attr) {
-    this.username = username;
+    this.id = attr.id;
+    this.username = attr.username;
+    this.buying_power = attr.buying_power;
+    this.wallet_value = attr.wallet_value;
   }
 
   save() {
     User.all.push(this);
   }
 
-  logout() {}
+  static logout() {
+    resetUserNav();
+    resetUserMain();
+    resetMarketMain();
+    User.current_user = null;
+    User.renderUserForm();
+    User.getUsers();
+    Coin.renderMarketTable();
+    Coin.getCoins();
+  }
 
   static create(attr) {
+    User.current_user = attr;
     let user = new User(attr);
     user.save();
     return user;
   }
 
   static createFromCollection(collection) {
-    collection.forEach((data) => User.create(data));
+    collection.forEach((data) => User.create(data.attributes));
+  }
+
+  static getWallet() {
+    User.renderUserWallet(
+      Trade.all.filter((t) => t.user_id === User.current_user.id)
+    );
   }
 
   /** Templates **/
 
-  // static userFormTemplate() {
-  //   // return `
-  //   //   <div class="row">
-  //   //     <div class="col-6">
-  //   //       <form class="pt-3" id="user-form">
-  //   //         <div class="mb-3">
-  //   //           <label for="username" class="form-label">Username</label>
-  //   //           <input type="text" class="form-control" name="username" id="username" />
-  //   //         </div>
-  //   //         <button type="submit" class="btn btn-sm btn-primary" >Submit</button>
-  //   //       </form>
-  //   //     </div>
-  //   //   </div>
-  //   // `;
-  //   let row = document.createElement("div");
-  //   row.className = "row";
+  static userFormTemplate() {
+    let form = document.createElement("form");
+    form.setAttribute("class", "d-flex");
+    form.setAttribute("id", "user-form");
+    form.addEventListener("submit", User.submitForm);
 
-  //   let col = document.createElement("div");
-  //   col.className = "col-6";
+    let input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.setAttribute("class", "form-control me-2");
+    input.setAttribute("name", "username");
+    input.setAttribute("id", "username");
+    input.setAttribute("placeholder", "Username");
 
-  //   let form = document.createElement("form");
-  //   form.className = "pt-3";
-  //   form.id = "user-form";
-  //   form.addEventListener("submit", User.submitForm);
+    let btn = document.createElement("button");
+    btn.setAttribute("type", "submit");
+    btn.setAttribute("class", "btn btn-sm btn-success");
+    // btn.setAttribute("style", "background-color: #109e92");
+    btn.innerText = "Login";
 
-  //   let label = document.createElement("label");
-  //   label.className = "form-label";
-  //   label.setAttribute("for", "username");
-  //   label.innerText = "Username";
+    form.appendChild(input);
+    form.appendChild(btn);
 
-  //   let input = document.createElement("input");
-  //   input.setAttribute("type", "text");
-  //   input.setAttribute("name", "username");
-  //   input.setAttribute("id", "username");
-  //   input.className = "form-control";
-
-  //   let btn = document.createElement("button");
-  //   btn.setAttribute("type", "submit");
-  //   btn.className = "btn btn-sm btn-primary mt-3";
-  //   btn.innerText = "Submit";
-
-  //   form.appendChild(label);
-  //   form.appendChild(input);
-  //   form.appendChild(btn);
-
-  //   col.appendChild(form);
-  //   row.appendChild(col);
-
-  //   userMain().appendChild(row);
-  // }
+    userNav().appendChild(form);
+  }
 
   static userInfoTemplate() {
     let uSpan = document.createElement("span");
-    let bpSpan = document.createElement("span");
-    let wvSpan = document.createElement("span");
+    let usr = document.createElement("span");
 
     uSpan.setAttribute("class", "navbar-text me-3");
     uSpan.innerHTML = "Username: ";
-
-    bpSpan.setAttribute("class", "navbar-text me-3");
-    bpSpan.innerHTML = "Buying Power: ";
-
-    wvSpan.setAttribute("class", "navbar-text me-3");
-    wvSpan.innerHTML = "Wallet Value: ";
-
-    let usr = document.createElement("span");
     usr.innerText = User.current_user.username;
     usr.style = "color: white";
 
+    let bpSpan = document.createElement("span");
     let bp = document.createElement("span");
+
+    bpSpan.setAttribute("class", "navbar-text me-3");
+    bpSpan.innerHTML = "Buying Power: ";
     bp.innerText = `$${numberWithCommas(
       parseFloat(User.current_user.buying_power).toFixed(2)
     )}`;
@@ -105,6 +93,16 @@ class User {
     )}`;
     wv.style = "color: white";
 
+    let wvSpan = document.createElement("span");
+    wvSpan.setAttribute("class", "navbar-text me-3");
+    wvSpan.innerHTML = "Wallet Value: ";
+
+    let l = document.createElement("button");
+    l.innerHTML = "Logout";
+    l.setAttribute("type", "submit");
+    l.setAttribute("class", "btn btn-sm btn-success");
+    l.addEventListener("click", User.logout);
+
     uSpan.appendChild(usr);
     bpSpan.appendChild(bp);
     wvSpan.appendChild(wv);
@@ -112,11 +110,12 @@ class User {
     userNav().appendChild(uSpan);
     userNav().appendChild(bpSpan);
     userNav().appendChild(wvSpan);
+    userNav().appendChild(l);
   }
 
   static walletTableTemplate() {
     return `
-      <h4 class="pt-3">CTS Wallet</h4>
+      <h4 class="pt-4">CTS Wallet</h4>
       <table class="table table-hover">
         <thead>
           <tr>
@@ -136,45 +135,49 @@ class User {
 
   /** Fetches **/
 
-  static async getUserInfo() {
-    const data = await Api.get("/users");
+  static async getUserInfo(id) {
+    const data = await Api.get(`/users/${id}`);
+    // add render
+  }
 
-    User.current_user = data.find(
-      (user) => user.username === User.current_user.username
-    );
+  static async getUsers() {
+    const users = await Api.get("/users");
+    User.all = [];
+    User.createFromCollection(users.data);
   }
 
   static submitForm(e) {
     e.preventDefault();
 
-    let strongParams = {
-      user: {
-        username: userFormInput().value,
-      },
-    };
+    let usr = User.all.find((u) => u.username === userFormInput().value);
 
-    fetch("http://localhost:3000/users", {
-      body: strongParams,
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(strongParams),
-      method: "POST",
-    })
-      .then(function (resp) {
-        return resp.json();
-      })
-      .then(function (data) {
-        User.current_user = data;
+    if (usr) {
+      User.current_user = usr;
+      User.renderUserInfo();
+      Coin.renderMarketTable();
+      User.getWallet();
+      Coin.getCoins();
+    } else {
+      let strongParams = {
+        user: {
+          username: userFormInput().value,
+        },
+      };
+
+      Api.post("/users", strongParams).then(function (data) {
+        User.create(data);
         User.renderUserInfo();
-        User.renderUserWallet();
+        User.getWallet();
+        Coin.renderM4arketTable();
+        Coin.getCoins();
       });
+    }
   }
 
   /** Renders **/
 
   static renderUserForm() {
+    resetUserNav();
     User.userFormTemplate();
   }
 
@@ -184,7 +187,93 @@ class User {
     User.userInfoTemplate();
   }
 
-  static renderUserWallet() {
-    userMain().innerHTML += User.walletTableTemplate();
+  static renderUserCoin(crypto) {
+    let tr = document.createElement("tr");
+
+    let logo = document.createElement("img");
+    logo.setAttribute("id", "logo");
+    logo.src = crypto.logo;
+
+    let coin = document.createElement("th");
+    coin.setAttribute("scope", "row");
+    coin.appendChild(logo);
+    coin.innerHTML += crypto.coin_id;
+
+    let sym = document.createElement("td");
+    sym.innerHTML = crypto.sym.toUpperCase();
+
+    let qty = document.createElement("td");
+    qty.innerHTML = crypto.qty;
+
+    let cost = document.createElement("td");
+    cost.innerHTML = `$${crypto.cost.toFixed(2)}`;
+
+    let val = document.createElement("td");
+    let current_val =
+      Coin.all.find((c) => c.id === crypto.coin_id).current_price * crypto.qty;
+    val.innerHTML = `$${current_val.toFixed(2)}`;
+
+    let r = document.createElement("td");
+    let r_val = crypto.cost - current_val;
+    r.innerHTML = `$${r_val.toFixed(2)}`;
+
+    if (r_val > 0.0) {
+      r.style.color = "green";
+    }
+
+    if (r_val < 0.0) {
+      r.style.color = "red";
+    }
+
+    let action = document.createElement("td");
+    let form = document.createElement("form");
+    form.setAttribute("class", "d-flex");
+    form.setAttribute("id", "sell-form");
+    // form.addEventListener("submit", function (e) {
+    //   e.preventDefault();
+
+    //   Trade.buy(User.current_user, crypto, buyQty(crypto.id).value);
+    //   // console.log(buyQty(crypto.id).value);
+    //   // console.log(crypto.id);
+    //   // console.log(crypto.current_price);
+    //   // debugger;
+    // });
+
+    let input = document.createElement("input");
+    input.setAttribute("type", "number");
+    input.setAttribute("class", "form-control me-2");
+    input.setAttribute("name", "qty");
+    input.setAttribute("id", `${crypto.coin_id}-qty`);
+    input.setAttribute("placeholder", "Qty");
+
+    let btn = document.createElement("button");
+    btn.setAttribute("type", "submit");
+    btn.setAttribute("class", "btn btn-sm btn-success");
+    btn.innerText = "Sell";
+
+    form.appendChild(input);
+    form.appendChild(btn);
+
+    // userNav().appendChild(form);
+
+    action.appendChild(form);
+
+    tr.appendChild(coin);
+    tr.appendChild(sym);
+    tr.appendChild(qty);
+    tr.appendChild(cost);
+    tr.appendChild(val);
+    tr.appendChild(r);
+    tr.appendChild(action);
+
+    walletTable().appendChild(tr);
+  }
+
+  static renderUserWallet(coins) {
+    userMain().innerHTML = User.walletTableTemplate();
+
+    coins.forEach(function (coin) {
+      User.renderUserCoin(coin);
+    });
   }
 }
